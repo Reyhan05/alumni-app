69a5a3bc9a73128119fa2fc2347a7292182a04ed
Reyhan05 <kingreyhan298@gmail.com>
2023-03-06 14:30:53 +0700

    create data table for student

--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -4,7 +4,26 @@
   <body>
     <h1>Data alumni</h1>
     <!-- membuat sebuah table -->
-    <div class="table-responsive"></div>
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>NIK</th>
+                    <th>Tanggal Lahir</th>
+                    <th>Jurusan</th>
+                    <th>Angkatan</th>
+                    <th>Alamat</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
     @include(layout.js)
   </body>
